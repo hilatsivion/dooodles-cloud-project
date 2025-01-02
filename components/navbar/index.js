4512c79add7c519eeae9068dcf3aa47126c7f187
hilatsivion <hilatsivion2@gmail.com>
2025-01-02 16:09:39 +0200

    update folders directory

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Load the navbar into the page
   const navbarContainer = document.getElementById("navbar-container");
   if (navbarContainer) {
-    fetch("../components/navbar/navbar.html")
+    fetch("../../components/navbar/navbar.html")
       .then((response) => response.text())
       .then((data) => {
         navbarContainer.innerHTML = data;
