86c199661c8b35659c0a407142bb31337cc07066
hilatsivion <hilatsivion2@gmail.com>
2025-01-02 23:03:31 +0200

    leadboard done

--- pages/Leadboard/leadboard.js
+++ pages/Leadboard/leadboard.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Simulate fetching leaderboard data (Replace this with actual server data fetch)
   const leaderboardData = [
-    { rank: 1, username: "User123", score: 1 },
-    { rank: 2, username: "User234", score: 2 },
-    { rank: 3, username: "Alony", score: 3 },
-    { rank: 4, username: "Gad123", score: 4 },
-    { rank: 5, username: "God", score: 5 },
+    { placerank: 1, username: "User123", place: 1 },
+    { rank: 2, username: "User234", place: 2 },
+    { rank: 3, username: "Alony", place: 3 },
+    { rank: 4, username: "Gad123", place: 4 },
+    { rank: 5, username: "God", place: 5 },
   ];
 
   // Display the leaderboard
@@ -13,16 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const challengeName = document.getElementById("challenge-name");
 
   // Set the challenge name (you can replace this with data from the server)
-  challengeName.textContent = "Dog walks on the moon";
+  challengeName.textContent = "Sample Sample";
 
   leaderboardData.forEach((entry) => {
     const leaderboardItem = document.createElement("div");
     leaderboardItem.classList.add("leaderboard-item");
 
     leaderboardItem.innerHTML = `
-        <div class="rank">${entry.rank}</div>
+        <div class="place">${entry.place}</div>
         <div class="username">${entry.username}</div>
-        <div class="score">${entry.score}</div>
+        <div class="place">${entry.place}</div>
       `;
 
     leaderboardContainer.appendChild(leaderboardItem);
